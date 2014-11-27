5ba631068660abb91bdd6d3f99e36e92259171d1
MonkeyRum <monkey.rum.github@gmail.com>
2014-11-27 03:38:40 +0000

    Added debug flag for showing images

--- m/findspuds.m
+++ m/findspuds.m
@@ -23,45 +23,64 @@
 %=========================================================================%
 
 function findspuds(I)
+
+DEBUG = 0;
+
 %% STEP 0
-figure; imshow(I); title('Original input');
+if(DEBUG)
+    figure; imshow(I); title('Original input');
+end
 
 %% STEP 1
 % Create a grey scale version of the image in order to better remove the
 % background noise, since the noise from all channels will coalesce
 GI = rgb_to_luminosity(I);
-figure; imshow(GI); title('Luminosity');
+if(DEBUG)
+    figure; imshow(GI); title('Luminosity');
+end
 
 % Use a median filter to remove noise artefacts
 MI = median_filter(GI, [3, 3]); % median filtered image
-figure; imshow(MI); title('Median filtered');
+if(DEBUG)
+    figure; imshow(MI); title('Median filtered');
+end
 
 FI = GI - MI; % filtered image
 GI = GI - FI;
-figure; imshow(GI); title('Filtered');
+if(DEBUG)
+    figure; imshow(GI); title('Filtered');
+end
 
 %% STEP 2
 % Create a black / white 'mask' that can be used to segment out the
 % potatoes from the background
 BW = GI > 20;
-figure; imshow(BW); title('Logical');
+if(DEBUG)
+    figure; imshow(BW); title('Logical');
+end
 
 % step 3 
 % clean up - fill potato
 %fill_image = imfill(BW, 'holes');
 % really slow because of Java import
 fill_image = fill_holes(BW);
-figure; imshow(fill_image); title('Fill holes');
+if(DEBUG)
+    figure; imshow(fill_image); title('Fill holes');
+end
 
 % step 4 
 % open some gaps
 se = strel('diamond',4);
 open_image = imopen(fill_image, se);
-figure; imshow(open_image); title('Open');
+if(DEBUG)
+    figure; imshow(open_image); title('Open');
+end
 
-% step 4
+% step 5
 % label potatoes
 [L, num] = bwlabel(open_image, 8);
 outline_image = label2rgb(L, 'lines', 'k', 'shuffle');
 outline_image = imdilate(outline_image,se) - outline_image;
-figure; imshow(outline_image); title('Outlines');
+if(DEBUG)
+    figure; imshow(outline_image); title('Outlines');
+end
