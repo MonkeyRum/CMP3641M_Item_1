a24bc429a5f7f6bf521ab7840a6dc9d484c8bd53
Alex Jenkins <monkey.rum.github@gmail.com>
2014-12-09 13:39:33 +0000

    Comments in remove_background
    Added comments and function decoration to remove_background.m
    
    See #12

--- m/remove_background.m
+++ m/remove_background.m
@@ -1,6 +1,22 @@
+%% =======================================================================%
+% remove_background.m                                                     %
+%=========================================================================%
+% Function:     remove_background                                         %
+% Author(s):    Alexander Jenkins (JEN11214787)                           %
+% Description:  Uses a background model from model_background.m to remove %
+%               the background of an image that contains the background   %
+% Returns:      Logical mask of background (zero) and foreground (one)    %
+%=========================================================================%
+
+%% =======================================================================%
+% remove_background                                                       %
+%                                                                         %
+% Arguments:                                                              %
+% IN(model)   Background model                                            %
+% IN(image)   Image to create background mask from                        %
+%=========================================================================%
+
 function [ logical_segment ] = remove_background( model, image )
-%UNTITLED3 Summary of this function goes here
-%   Detailed explanation goes here
 
 WHD = size(image);
 w = WHD(1);
@@ -15,11 +31,12 @@ for y=1:h
         g = image(x, y, 2) + 1;
         b = image(x, y, 3) + 1;
         
+        % remove background if the colour is set in the model
         if model(r, g, b)
             logical_segment(x, y) = 0;
         end
-                
-        str = ['reference frame: [', num2str(x), ',', num2str(y), ']\n'];
+
+        str = ['reference pixel: [', num2str(x), ',', num2str(y), ']\n'];
         fprintf(str);
         
     end
